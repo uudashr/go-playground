d0b30b9ed7cf4ce5e6a9cc930114c718e4f77dfd
Nuruddin Ashr <uudashr@gmail.com>
2020-07-14 22:58:04 +0700

    Add test for json webtoken

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/uudashr/go-playground
 go 1.14
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/go-chi/chi v4.1.1+incompatible
 	github.com/go-chi/cors v1.1.1
 	github.com/gorilla/handlers v1.4.2
---
+++ jwebtoken/jwt_test.go
@@ -0,0 +1,41 @@
+package jwebtoken_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+func TestSigning(t *testing.T) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub":  "1234567890",
+		"name": "John Doe",
+		"iss":  "Jungle",
+		"aud":  "jungleapp",
+		"iat":  time.Now().Unix(),
+	})
+	secretKey := []byte("secret")
+	tokenString, err := token.SignedString(secretKey)
+	if err != nil {
+		t.Fatal("Fail to sign:", err)
+	}
+
+	jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return secretKey, nil
+	})
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		t.Fatal("Invalid token")
+	}
+
+	if got, want := claims["sub"], "1234567890"; got != want {
+		t.Errorf("Claim sub got: %q, want: %q", got, want)
+	}
+}
