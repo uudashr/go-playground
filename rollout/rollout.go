1c9cffb8073bcd7970275d085f7c2d44bb7cab39
Nuruddin Ashr <uudashr@gmail.com>
2023-03-09 11:25:07 +0700

    Simplify rollout logic

--- rollout/rollout.go
+++ rollout/rollout.go
@@ -1,6 +1,9 @@
 package rollout
 
-import "hash/fnv"
+import (
+	"errors"
+	"hash/fnv"
+)
 
 func ShouldRollout(id string, ratio float32) (bool, error) {
 	h := fnv.New32()
@@ -17,13 +20,57 @@ func ShouldRollout(id string, ratio float32) (bool, error) {
 }
 
 func DistributionIndex(id string, ratios []float32) (int, error) {
+	if len(ratios) == 0 {
+		return -1, errors.New("ratios is empty")
+	}
+
+	if err := validateRatios(ratios); err != nil {
+		return -1, err
+	}
+
+	var sumRatio float32
+	for _, ratio := range ratios {
+		if ratio < 0 || ratio > 1 {
+			return -1, errors.New("ratio must be in [0, 1]")
+		}
+		sumRatio += ratio
+	}
+
 	h := fnv.New32()
 	if _, err := h.Write([]byte(id)); err != nil {
 		return -1, err
 	}
 
-	sum := h.Sum32()
-	mod := sum % uint32(segmentSize)
+	hashVal := h.Sum32()
+	segmentIndex := int(hashVal % 100) // put into segments [0 - 99]
 
-	return distributionIndex(int(mod), ratios)
+	var limit int
+	for i, ratio := range ratios[:len(ratios)-1] {
+		limit += int(ratio * 100)
+		if segmentIndex <= limit {
+			return i, nil
+		}
+	}
+
+	return len(ratios) - 1, nil
+}
+
+func validateRatios(ratios []float32) error {
+	if len(ratios) == 0 {
+		return errors.New("ratios is empty")
+	}
+
+	var sumRatio float32
+	for _, ratio := range ratios {
+		if ratio < 0 || ratio > 1 {
+			return errors.New("ratio must be in [0, 1]")
+		}
+		sumRatio += ratio
+	}
+
+	if sumRatio != 1 {
+		return errors.New("sum of ratios must be 1")
+	}
+
+	return nil
 }
