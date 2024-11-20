1e3e1ae726dab2dcc55da575778f6189e0698d10
Nuruddin Ashr <uudashr@gmail.com>
2024-11-20 15:04:00 +0700

    Use numbers

--- cmd/locale/main.go
+++ cmd/locale/main.go
@@ -5,12 +5,14 @@ import (
 
 	"golang.org/x/text/language"
 	"golang.org/x/text/message"
+	"golang.org/x/text/number"
 )
 
 // ref: https://pkg.go.dev/golang.org/x/text
 
 func main() {
 	discount := 0.257
+	fmt.Println("Discount:", discount)
 
 	msg := &Message{}
 
@@ -37,6 +39,10 @@ func main() {
 	// Print in ar-SA locale and language
 	// Output: تم تطبيق خصم بنسبة ٢٥٫٧٪
 	msg.DiscountApplied(discount, "ar-SA")
+
+	// Print in ar-AE locale and language
+	// Output: ٢٥٫٧٪ discount applied
+	msg.DiscountApplied(discount, "ar-AE")
 
 	// Print in en-US locale and language
 	// Output: Good morning
@@ -71,24 +77,43 @@ func (m *Message) DiscountApplied(discount float64, locale string) {
 		p = message.NewPrinter(language.French)
 	case "ar-SA":
 		p = message.NewPrinter(language.Arabic)
+	case "ar-AE":
+		p = message.NewPrinter(language.Arabic)
 	default:
 		p = message.NewPrinter(language.AmericanEnglish)
 	}
 
 	switch locale {
 	case "en-US", "en-GB":
-		p.Printf("%.1f%% discount applied\n", discount*100)
+		p.Printf("%v discount applied\n", number.Percent(discount))
 	case "id-ID":
-		p.Printf("Diskon %.1f%% diterapkan\n", discount*100)
+		p.Printf("Diskon %v diterapkan\n", number.Percent(discount))
 	case "de-DE":
-		p.Printf("%.1f%% Rabatt angewendet\n", discount*100)
+		p.Printf("%v Rabatt angewendet\n", number.Percent(discount))
 	case "fr-FR":
-		p.Printf("%.1f%% de réduction appliquée\n", discount*100)
+		p.Printf("%v de réduction appliquée\n", number.Percent(discount))
 	case "ar-SA":
-		p.Printf("تم تطبيق خصم بنسبة %.1f%%\n", discount*100)
+		p.Printf("تم تطبيق خصم بنسبة %v\n", number.Percent(discount))
+	case "ar-AE":
+		p.Printf("%v discount applied\n", number.Percent(discount))
 	default:
-		p.Printf("%.1f%% discount applied\n", discount*100)
+		p.Printf("%v discount applied\n", number.Percent(discount))
 	}
+
+	// switch locale {
+	// case "en-US", "en-GB":
+	// 	p.Printf("%.1f%% discount applied\n", discount*100)
+	// case "id-ID":
+	// 	p.Printf("Diskon %.1f%% diterapkan\n", discount*100)
+	// case "de-DE":
+	// 	p.Printf("%.1f%% Rabatt angewendet\n", discount*100)
+	// case "fr-FR":
+	// 	p.Printf("%.1f%% de réduction appliquée\n", discount*100)
+	// case "ar-SA":
+	// 	p.Printf("تم تطبيق خصم بنسبة %.1f%%\n", discount*100)
+	// default:
+	// 	p.Printf("%.1f%% discount applied\n", discount*100)
+	// }
 }
 
 func (m *Message) Greeting(locale string) {
