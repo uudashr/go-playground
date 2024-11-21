94ae84aa487e5b153e2b03e51645dd55c9dcb340
Nuruddin Ashr <uudashr@gmail.com>
2024-11-21 13:33:38 +0700

    Add new language

--- cmd/promotion/main.go
+++ cmd/promotion/main.go
@@ -14,16 +14,16 @@ func main() {
 	discount := 0.257457
 	fixed := 20.3456
 
-	// Print in en-US locale and language
+	// Print in us-US locale and language
 	// Output: 26% discount applied
 	// Output: USD 20.35 fixed amount
-	en, _ := language.Parse("en-US")
-	enp := message.NewPrinter(en)
-	enp.Printf("Discount %v applied\n", number.Percent(discount))
+	us, _ := language.Parse("en-US")
+	usp := message.NewPrinter(us)
+	usp.Printf("Discount %v applied\n", number.Percent(discount))
 
 	enUnit, _ := currency.ParseISO("USD")
 	formatEn := currency.ISO.Default(enUnit).Kind(currency.Accounting)
-	enp.Printf("%v fixed amount\n", formatEn(fixed))
+	usp.Printf("%v fixed amount\n", formatEn(fixed))
 
 	fmt.Println("----------------")
 
@@ -40,14 +40,27 @@ func main() {
 
 	fmt.Println("----------------")
 
-	// Print in ar-AE locale and language
+	// Print in ae-AE locale and language
 	// Output: تم تطبيق خصم 26%
 	// Output: AED 20.35 المبلغ الثابت
-	ar, _ := language.Parse("ar-AE")
-	arp := message.NewPrinter(ar)
-	arp.Printf("تم تطبيق خصم %v\n", number.Percent(discount))
+	ae, _ := language.Parse("ar-AE")
+	aep := message.NewPrinter(ae)
+	aep.Printf("تم تطبيق خصم %v\n", number.Percent(discount))
 
 	arUnit, _ := currency.ParseISO("AED")
 	formatAr := currency.ISO.Default(arUnit).Kind(currency.Accounting)
-	arp.Printf("%v المبلغ الثابت\n", formatAr(fixed))
+	aep.Printf("%v المبلغ الثابت\n", formatAr(fixed))
+
+	fmt.Println("----------------")
+
+	// Print in ar-JO locale and language
+	// Output: تم تطبيق خصم 26%
+	// Output: JOD 20.35 المبلغ الثابت
+	jo, _ := language.Parse("ar-JO")
+	jop := message.NewPrinter(jo)
+	jop.Printf("تم تطبيق خصم %v\n", number.Percent(discount))
+
+	joUnit, _ := currency.ParseISO("JOD")
+	formatJo := currency.ISO.Default(joUnit).Kind(currency.Accounting)
+	jop.Printf("%v المبلغ الثابت\n", formatJo(fixed))
 }
